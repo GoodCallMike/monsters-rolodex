ed6d070f6c83114aceeb4f8428134413b8c8ad0c
Michael Saylor <michael_saylor@carmax.com>
2022-04-16 10:08:15 -0400

    add css font styles

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -8,11 +8,12 @@ class Card extends Component {
     return (
       <div className="card-container" key={id}>
         <img
+          className="monster-image"
           alt={`monster ${name}`}
           src={`https://robohash.org/${id}?set=set2&size=180x180`}
         />
-        <h2>{name}</h2>
-        <p>{email}</p>
+        <h2 className="monster-name">{name}</h2>
+        <p className="monster-email">{email}</p>
       </div>
     );
   }
